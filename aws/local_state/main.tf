281a894ca37922deb03fffadd738bc3a15fadfee
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2023-06-05 16:57:47 +0100

    added the configuration and resource to created

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -1,1 +1,36 @@
 #create AWS EC2 Instance
+
+
+terraform {
+
+# provider details=> console=>  terraform init
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+
+
+# Configure the AWS Provider
+provider "aws" {
+  region  = "us-west-2"
+}
+
+
+#Resource to be created
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "BB-server with Terraform"
+  }
+
+
+}
